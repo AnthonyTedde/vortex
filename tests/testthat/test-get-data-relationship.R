77efd5c2b8fd3b5d017d08b93d152d770d07bd26
AnthonyTedde <antho.tedde@gmail.com>
2019-09-02 17:02:14 +0200

    Make test case more general

--- tests/testthat/test-get-data-relationship.R
+++ tests/testthat/test-get-data-relationship.R
@@ -15,17 +15,22 @@ test_that("Data is bound by column (based on id)", {
   # Create the subset data.frame
   d1 <- head(mtcars)[, 1:2]
   d2 <- head(mtcars, 8)[, 3:4]
+  d3 <- head(mtcars, 20)[, 5:7]
   # Add unique identifier for each row of each data.frame
   d1["id"] <- 1:nrow(d1)
   d2["id"] <- 1:nrow(d2)
+  d3["id"] <- 1:nrow(d3)
   # Create arguments list
   arguments <- list(data1 = d1,
-                    data2 = d2)
+                    data2 = d2,
+                    data3 = d3)
   returned <- vortex::get_data_relationship(arguments, by = "id")
 
   d1 <- tibble::rownames_to_column(d1, var = "data1.rowname")
   d2 <- tibble::rownames_to_column(d2, var = "data2.rowname")
-  expected <- dplyr::inner_join(d1, d2, by = "id")
+  d3 <- tibble::rownames_to_column(d3, var = "data3.rowname")
+  expected <- dplyr::inner_join(d1, d2, by = "id") %>%
+    dplyr::inner_join(d3, by = "id")
   expect_equal(returned, expected)
 })
 
